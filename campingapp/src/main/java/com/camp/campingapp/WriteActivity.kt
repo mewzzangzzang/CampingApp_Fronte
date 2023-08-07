729ae26768944c8f9717ec0d728789cb8cd00b1e
JMY00n <3zlll0@naver.com>
2023-08-07 13:51:51 +0900

    파이어베이스에 데이터 저장 기능 추가

--- campingapp/src/main/java/com/camp/campingapp/WriteActivity.kt
+++ campingapp/src/main/java/com/camp/campingapp/WriteActivity.kt
@@ -2,28 +2,27 @@ package com.camp.campingapp
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.camp.campingapp.databinding.ActivityWriteBinding
 import com.camp.campingapp.util.dateToString
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import java.io.File
 import java.util.Date
 
 
 class WriteActivity : AppCompatActivity() {
-    val db = Firebase.firestore
+    lateinit var db: FirebaseFirestore
+    lateinit var storage: FirebaseStorage
 
     lateinit var binding: ActivityWriteBinding
     lateinit var filePath: String
@@ -33,93 +32,79 @@ class WriteActivity : AppCompatActivity() {
         binding = ActivityWriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = FirebaseFirestore.getInstance()
+        storage = Firebase.storage
+
         binding.postbtn.setOnClickListener {
-            val data = mapOf(
-                "title" to binding.title.text.toString(),
-                "content" to binding.addEditView.text.toString(),
-                "date" to dateToString(Date())
-            )
-            db.collection("Boards")
-                .add(data)
-                .addOnSuccessListener {
-                    Toast.makeText(this, "게시글 작성 완료", Toast.LENGTH_SHORT).show()
-                }
-                .addOnFailureListener {
-                    Toast.makeText(this, "error!!", Toast.LENGTH_SHORT).show()
-                }
+            saveStore()
+        }
+
+        binding.upload.setOnClickListener {
+                val intent = Intent(Intent.ACTION_PICK)
+                intent.setDataAndType(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    "image/*"
+                )
+                requestLauncher.launch(intent)
+
         }
 
     }
 
-//    val requestLauncher = registerForActivityResult(
-//        ActivityResultContracts.StartActivityForResult())
-//    {
-//        if(it.resultCode === android.app.Activity.RESULT_OK){
-//            Glide
-//                .with(getApplicationContext())
-//                .load(it.data?.data)
-//                .apply(RequestOptions().override(250, 200))
-//                .centerCrop()
-//                .into(binding.addImageView)
-//
-//            val cursor = contentResolver.query(it.data?.data as Uri,
-//                arrayOf<String>(MediaStore.Images.Media.DATA), null, null, null);
-//            cursor?.moveToFirst().let {
-//                filePath=cursor?.getString(0) as String
-//            }
-//        }
-//    }
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-////        menuInflater.inflate(R.menu.menu_write, menu)
-//        return super.onCreateOptionsMenu(menu)
-//    }
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        if(item.itemId === R.id.menu_add_gallery){
-//            val intent = Intent(Intent.ACTION_PICK)
-//            intent.setDataAndType(
-//                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-//                "image/*"
-//            )
-//            requestLauncher.launch(intent)
-//        }else if(item.itemId === R.id.menu_add_save){
-//            if(binding.addEditView.text.isNotEmpty()){
-//                saveStore()
-//                Toast.makeText(this, "작성 완료.", Toast.LENGTH_SHORT).show()
-//            }else {
-//                Toast.makeText(this, "글을 입력해 주세요.", Toast.LENGTH_SHORT).show()
-//            }
-//
-//        }
-//        return super.onOptionsItemSelected(item)
+    private val requestLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode === android.app.Activity.RESULT_OK) {
+            Glide
+                .with(applicationContext)
+                .load(it.data?.data)
+                .apply(RequestOptions().override(250, 200))
+                .centerCrop()
+                .into(binding.imageView)
+            val cursor = contentResolver.query(
+                it.data?.data as Uri,
+                arrayOf<String>(MediaStore.Images.Media.DATA), null, null, null
+            )
+            cursor?.moveToFirst().let {
+                filePath = cursor?.getString(0) as String
+            }
+        }
     }
 
-//    private fun saveStore(){
-//        val data = mapOf(
-//            "content" to binding.addEditView.text.toString(),
-//            "date" to dateToString(Date())
-//        )
-//        MyApplication.db.collection("boards")
-//            .add(data)
-//            .addOnSuccessListener {
-//                uploadImage(it.id)
-//            }
-//            .addOnFailureListener{
-//                Log.d("kkang", "data save error", it)
-//            }
-//    }
-//    private fun uploadImage(docId: String){
-//        val storage = MyApplication.storage
-//        val storageRef = storage.reference
-//        val imgRef = storageRef.child("images/${docId}.jpg")
-//
-//        val file = Uri.fromFile(File(filePath))
-//        imgRef.putFile(file)
-//            .addOnSuccessListener {
-//                Toast.makeText(this, "save ok..", Toast.LENGTH_SHORT).show()
-//                finish()
-//            }
-//            .addOnFailureListener{
-//                Log.d("kkang", "file save error", it)
-//            }
-//
-//    }
+    private fun saveStore() {
+        val data = mapOf(
+            "title" to binding.title.text.toString(),
+            "content" to binding.addEditView.text.toString(),
+            "date" to dateToString(Date())
+        )
+        db.collection("Boards")
+            .add(data)
+            .addOnSuccessListener {
+                uploadImage(it.id)
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "error!!", Toast.LENGTH_SHORT).show()
+            }
+        finish()
+    }
+
+    private fun uploadImage(docId: String) {
+        val storage = storage
+        val storageRef = storage.reference
+        val imgRef = storageRef.child("images/${docId}.jpg")
+
+        val file = Uri.fromFile(File(filePath))
+        imgRef.putFile(file)
+            .addOnSuccessListener {
+                Toast.makeText(this, "upload ok", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "upload fail", Toast.LENGTH_SHORT).show()
+
+            }
+    }
+
+}
+
+
--- campingapp/src/main/java/com/camp/campingapp/model/ItemData.kt
+++ campingapp/src/main/java/com/camp/campingapp/model/ItemData.kt
@@ -4,4 +4,6 @@ class ItemData {
     var docId: String? = null
     var content: String? = null
     var date: String? = null
+//    var imageUrl : String? = null
+
 }
