32f2d64af7c4a8cbb0832203f31523b228ed4888
Song <scw387@naver.com>
2023-08-09 17:35:16 +0900

    일반회원,호스트로그인 복구

--- campingapp/src/main/java/com/camp/campingapp/recycler/MyAdapter.kt
+++ campingapp/src/main/java/com/camp/campingapp/recycler/MyAdapter.kt
@@ -9,13 +9,14 @@ import com.camp.campingapp.test.MyApplication
 import com.camp.campingapp.databinding.ItemMainBinding
 import com.camp.campingapp.model.ItemData
 
+
 //리사이클러 뷰->목록 형식으로 출력되는 뷰
 //뷰홀더->뷰 객체들의 모음집. 해당 뷰 홀더의 주 생성자의 매개변수에,바인딩 기법으로 객체가 선언됨
 //그래서,해당 매개변수호,전체 뷰 객체에 접근 가능(뷰바인딩 객체는,목록의 아이템의 요소 사용중)
 class MyViewHolder(val binding: ItemMainBinding) : RecyclerView.ViewHolder(binding.root)
 
 class MyAdapter(val context: Context, val itemList: MutableList<ItemData>): RecyclerView.Adapter<MyViewHolder>() {
-//어댑터 클래스를 만들고,재정의한 함수들임
+    //어댑터 클래스를 만들고,재정의한 함수들임
     //인플레이터가 나옴,해당 뷰를 출력하기 위한 객체를 초기화하는 작업\
     //포기화는(참조형 변수에,해당 메모리 위치 주솟값을 할당하는것에 말함)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -35,13 +36,14 @@ class MyAdapter(val context: Context, val itemList: MutableList<ItemData>): Recy
 
         holder.binding.run {
             itemEmailView.text=data.email
+            itemTypeView.text=data.type
         }
 
         //스토리지 이미지 다운로드........................
         //MyApplication-매니페스트에 등록이 되어있음
         //imgRef 이 객체를 싸용해서,업로드,다운로드 구현
         //다운로드가 잘되면 ,콜백으로 돌아와서 로직 실행
-                //스토리지에서 이미지의 url 주소만 가져온다
+        //스토리지에서 이미지의 url 주소만 가져온다
         val imgRef = MyApplication.storage.reference.child("images/${data.uid}.jpg")
         imgRef.downloadUrl.addOnCompleteListener{ task ->
 
