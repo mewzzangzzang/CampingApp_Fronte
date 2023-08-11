02997f16fd456501a79c5b1695541a3ecaa00c1d
Xenodap <scw387@naver.com>
2023-08-11 12:11:12 +0900

    송치원

--- campingapp/src/main/java/com/camp/campingapp/HostListActivity.kt
+++ campingapp/src/main/java/com/camp/campingapp/HostListActivity.kt
@@ -3,23 +3,24 @@ package com.camp.campingapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import com.camp.campingapp.databinding.ActivityBoardBinding
+import com.camp.campingapp.databinding.ActivityHostListBinding
 import com.camp.campingapp.util.myCheckPermission
 
-class BoardActivity : AppCompatActivity() {
-    lateinit var binding: ActivityBoardBinding
+class HostListActivity: AppCompatActivity() {
+    lateinit var binding: ActivityHostListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityBoardBinding.inflate(layoutInflater)
+        binding = ActivityHostListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         myCheckPermission(this)
 
-        //
-//        binding.add.setOnClickListener {
-//            startActivity(Intent(this, WriteActivity::class.java))
-//        }
+        binding.add.setOnClickListener {
+            startActivity(Intent(this, WriteActivity::class.java))
+        }
 
     }
 }
