48a8651edfbea6e5292e548d1b52cd04563f702b
hanspung <hanspung@gmail.com>
2023-09-01 10:49:59 +0900

    게시글 삭제 기능 정상화

--- campingapp/src/main/java/com/camp/campingapp/BoardDelete.kt
+++ campingapp/src/main/java/com/camp/campingapp/BoardDelete.kt
@@ -1,21 +1,30 @@
 package com.camp.campingapp
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.camp.campingapp.databinding.ActivityBoardDeleteBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class BoardDelete : AppCompatActivity() {
     private lateinit var binding: ActivityBoardDeleteBinding
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBoardDeleteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        database = FirebaseDatabase.getInstance().reference
+
         val docId = intent.getStringExtra("DocId")
 
         if (docId != null) {
+            // 삭제 작업 진행
             deleteDatabaseAndNavigateToBoard(docId)
         } else {
             handleDeleteError()
@@ -23,24 +32,30 @@ class BoardDelete : AppCompatActivity() {
     }
 
     private fun deleteDatabaseAndNavigateToBoard(docId: String) {
-        deleteDatabase(docId)
-        showToastAndNavigate("게시글 삭제 완료", Board::class.java)
+        database.child("Boards").child(docId)
+            .removeValue()
+            .addOnSuccessListener {
+                showToastAndNavigate("게시글 삭제 완료")
+            }
+            .addOnFailureListener { exception ->
+                Log.e("BoardDelete", "Error deleting document: $docId", exception)
+                handleDeleteError()
+            }
     }
 
     private fun handleDeleteError() {
         showToast("게시글 삭제 실패")
-        // 추가적인 오류 처리 로직을 여기에 추가할 수 있음
-    }
-
-    private fun showToastAndNavigate(message: String, destination: Class<*>) {
-        showToast(message)
-        startActivity(Intent(this, destination).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        })
+        setResult(Activity.RESULT_CANCELED) // 삭제 실패 시 RESULT_CANCELED 설정
         finish()
     }
 
-    private fun showToast(message: String) {
+    private fun showToastAndNavigate(message: String) {
         showToast(message)
+        setResult(Activity.RESULT_OK) // 삭제 성공 시 RESULT_OK 설정
+        finish() // 액티비티 종료
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
