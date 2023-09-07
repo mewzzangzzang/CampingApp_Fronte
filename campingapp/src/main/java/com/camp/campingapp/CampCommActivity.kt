3588485b05973734ffb7a55217b12097996c8724
doumin <aishi1026@gmail.com>
2023-09-07 20:07:57 +0900

    일반캠핑장 리스트 추가

---
+++ campingapp/src/main/java/com/camp/campingapp/CampCommActivity.kt
@@ -0,0 +1,83 @@
+package com.camp.campingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.camp.campingapp.databinding.ActivityCampCommBinding
+import com.camp.campingapp.model.GlampList
+import com.camp.campingapp.recycler.CommListAdapter
+import com.camp.campingapp.recycler.GlampingListAdapter
+import retrofit2.Response
+
+class CampCommActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityCampCommBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityCampCommBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.CommRecyclerView.setOnClickListener{
+        }
+
+        val induty = "2"
+        val donm = intent.getStringExtra("donm")
+        val sigunguNm = intent.getStringExtra("sigunguNm")
+
+        if (donm != null && sigunguNm != null) {
+            getGlampingList(induty,donm, sigunguNm)
+            binding.where.text = sigunguNm
+        } else {
+            getGlampingList(induty,"1", "강릉시")
+            binding.where.text = "강릉시"
+        }
+
+        binding.location.setOnClickListener {
+            val intent = Intent(this@CampCommActivity, CommLocationActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    fun getGlampingList(induty:String, donm: String, sigunguNm: String) {
+
+        Log.d("dum"," getGlampingList 실행 ${induty} ${donm} ${sigunguNm} ")
+
+        var induty: String = induty
+        var donm: String = donm
+        var sigunguNm: String = sigunguNm
+        val networkService = (applicationContext as MyApplication).networkService
+        val userListCall =
+            networkService.campList(induty, donm, sigunguNm)
+
+        userListCall.enqueue(object : retrofit2.Callback<List<GlampList>> {
+            override fun onResponse(
+                call: retrofit2.Call<List<GlampList>>,
+                response: Response<List<GlampList>>
+            ) {
+                Log.d("dum", "glampList 들어옴")
+                if (response.isSuccessful) {
+                    val glampList = response.body()
+
+                    binding.CommRecyclerView.adapter =
+                        CommListAdapter(this@CampCommActivity, glampList)
+
+                    binding.CommRecyclerView.addItemDecoration(
+                        DividerItemDecoration(this@CampCommActivity, LinearLayoutManager.VERTICAL)
+                    )
+                } else {
+                    Log.e("dum", "Response not successful: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: retrofit2.Call<List<GlampList>>, t: Throwable) {
+                Log.d("dum", "실패")
+                call.cancel()
+            }
+        })
+    }
+
+}
---
+++ campingapp/src/main/java/com/camp/campingapp/commFragment/CommFragment16.kt
@@ -0,0 +1,83 @@
+package com.camp.campingapp.commFragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.camp.campingapp.CampCommActivity
+import com.camp.campingapp.R
+
+
+class CommFragment16 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_comm16, container, false)
+        var donm = "16"
+        var sigunguNm: String
+
+        val button1: Button = view.findViewById(R.id.button16_1)
+        button1.setOnClickListener {
+            Log.d("dum", "click")
+
+            sigunguNm = "대덕구"
+            val intent = Intent(activity, CampCommActivity::class.java)
+            intent.putExtra("donm", donm)
+            intent.putExtra("sigunguNm", sigunguNm)
+            startActivity(intent)
+        }
+
+        val button2: Button = view.findViewById(R.id.button16_2)
+        button2.setOnClickListener {
+            Log.d("dum", "click")
+
+            sigunguNm = "동구"
+            val intent = Intent(activity, CampCommActivity::class.java)
+            intent.putExtra("donm", donm)
+            intent.putExtra("sigunguNm", sigunguNm)
+            startActivity(intent)
+        }
+
+        val button3: Button = view.findViewById(R.id.button16_3)
+        button3.setOnClickListener {
+            Log.d("dum", "click")
+
+            sigunguNm = "서구"
+            val intent = Intent(activity, CampCommActivity::class.java)
+            intent.putExtra("donm", donm)
+            intent.putExtra("sigunguNm", sigunguNm)
+            startActivity(intent)
+        }
+
+        val button4: Button = view.findViewById(R.id.button16_4)
+        button4.setOnClickListener {
+            Log.d("dum", "click")
+
+            sigunguNm = "유성구"
+            val intent = Intent(activity, CampCommActivity::class.java)
+            intent.putExtra("donm", donm)
+            intent.putExtra("sigunguNm", sigunguNm)
+            startActivity(intent)
+        }
+
+        val button5: Button = view.findViewById(R.id.button16_5)
+        button5.setOnClickListener {
+            Log.d("dum", "click")
+
+            sigunguNm = "중구"
+            val intent = Intent(activity, CampCommActivity::class.java)
+            intent.putExtra("donm", donm)
+            intent.putExtra("sigunguNm", sigunguNm)
+            startActivity(intent)
+        }
+
+        return view
+    }
+
+}
